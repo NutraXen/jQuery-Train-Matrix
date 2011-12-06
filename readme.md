c75f0e8bacd216e7ec25cea5711f682ec4cc0afd
Jedidiah Broadbent <hello@jedidiah.eu>
2011-12-06 14:55:26 +0000

    Initial Import
    Another old project moving up to Github for new life and updates.

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Train Matrix jQuery Plugin #
+
+Train Matrix is a jQuery plugin that converts letters into a matrix like those used on old _First Great Western_ trains. The text remains selectable and displays normally without styles or javascript. It was inspired by [an article on Ministry of Type](http://ministryoftype.co.uk/words/article/serene_infoboards/ "Serene Infoboards | The Ministry of Type") by [Aegir Hallmundur](http://aegir.me/ "Aegir Hallmundur"). Aegir has mocked up the matrix and alphabet in Illustrator.
+
+Train Matrix is relatively light, it only uses one 3KB image, and the minified css and javascript together come to 16KB. This is still a very early version (though was written about 4 years ago and until recently was forgotten), but you can fork or download the source (including the minified versions). At the moment this plugin hasn't been tested in anything so I wouldn't be surprised if it breaks.
+
+
+## Examples ##
+
+To use the plugin you first need to upload the files _jquery.trainmatrix.min.js_ and _matrix-min.css_ to your server and include links to them in your HTML. You will also need to upload _matrix-20_20.png_ and depending on where you put the files you will have to update the link to the image in the CSS file. It is only referenced once in the CSS right at the top of the minified CSS or in the _matrix-base.css_ in the un-minified files.
+
+
+```html
+<link rel="stylesheet" type="text/css" href="matrix-min.css" media="all">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="jquery.trainmatrix.js"></script>
+```
+
+To apply the effect to an element:
+```javascript
+$(element).trainmatrix();
+```
+To apply the effect to an element with a custom string:
+```javascript
+$(element).trainmatrix({matrixText: 'This text will be used to generate the matrix'});
+```
+
+## Limitations ##
+
+There are currently no numbers. The only punctuation that is currently supported are the characters . , &ldquo; &rdquo; &#x27; : + - [ ] ( ) { } &bull; = _ ; *
+
+
+Stick to A-Z and basic punctuation and you should be fine.
---
+++ source/jquery.trainmatrix.js
@@ -0,0 +1,93 @@
+/*
+----------------------------------------
+
+Train Matrix jQuery Plugin
+
+	by Jedidiah Broadbent
+	http://jedidiah.eu/
+
+	October 2009
+
+----------------------------------------
+
+	Inspired by the post by Aegir Hallmundur at
+	http://ministryoftype.co.uk/words/article/serene_infoboards/
+
+----------------------------------------
+
+	TODO : Set Board Length
+	TODO : Animate Overflow and Transitions
+	TODO : Add in extra row above for diacritics eg. ö
+	TODO : Build Matrix from Array or List with each item as a 'screen'
+	TODO : Add options for controlling background colour
+	TODO : Support for character entities eg: &rsquo;
+	TODO : Support for nested tags
+
+----------------------------------------
+*/
+$.fn.trainmatrix = function(options) {
+// Default Settings
+	var defaults = {
+		matrixText: this.html()
+	};
+
+	var opts = $.extend(defaults, options);
+
+	// charToMatrix : Takes an individual char and returns the grid for that letter
+	function charToMatrix(theChar){
+		
+		// What kind of char is it?
+		switch(theChar){
+		case '&': theClassChar='punc_amp'; theChar='&amp;'; break;
+		case '!': theClassChar='punc_exc'; break;
+		case '.': theClassChar='punc_period'; break;
+		case ',': theClassChar='punc_comma'; break;
+		case '"': theClassChar='punc_sdquo'; break;
+		case "'": theClassChar='punc_rsquo'; break;
+		case '“': theClassChar='punc_ldquo'; break;
+		case '”': theClassChar='punc_rdquo'; break;
+		case '•': theClassChar='punc_bull'; break;
+		case '*': theClassChar='punc_star'; break;
+		case '^': theClassChar='punc_carrot'; break;
+		case '=': theClassChar='punc_equals'; break;
+		case '+': theClassChar='punc_plus'; break;
+		case '-': theClassChar='punc_minus'; break;
+		case '_': theClassChar='punc_under'; break;
+		case '/': theClassChar='punc_slash'; break;
+		case '\\':theClassChar='punc_bslash'; break;
+		case '<': theClassChar='punc_gt'; break;
+		case '>': theClassChar='punc_lt'; break;
+		case '£': theClassChar='punc_pound'; break;
+		case '@': theClassChar='punc_at'; break;
+		case '(': theClassChar='punc_obrac'; break;
+		case ')': theClassChar='punc_cbrac'; break;
+		case '[': theClassChar='punc_obrac'; break;
+		case ']': theClassChar='punc_cbrac'; break;
+		case '{': theClassChar='punc_ocbrac'; break;
+		case '}': theClassChar='punc_ccbrac'; break;
+		case ':': theClassChar='punc_colo'; break;
+		case ';': theClassChar='punc_scolo'; break;
+		
+		default: theClassChar='let_'+theChar;
+			
+		}
+		
+		// Return the markup for the char
+		return '<span class="letter '+theClassChar.toLowerCase()+'"><span class="col1"><span class="row1">'+theChar+'</span><span class="row2"></span><span class="row3"></span><span class="row4"></span><span class="row5"></span></span><span class="col2"><span class="row1"></span><span class="row2"></span><span class="row3"></span><span class="row4"></span><span class="row5"></span></span><span class="col3"><span class="row1"></span><span class="row2"></span><span class="row3"></span><span class="row4"></span><span class="row5"></span></span></span>';
+	}
+	
+	// convertToMatrix : Takes a string and passes each char to charToMatrix() then returns the string of grids
+	function convertToMatrix(theString){
+		var replacementString = '';
+		$.each(theString, function(index) {
+			replacementString += charToMatrix(this);
+		});
+		return replacementString;
+	}
+
+	// Takes the element and replaces it's content with the charecters as grids
+	this.html(convertToMatrix(opts.matrixText));
+	
+	// Returns The jQuery Object
+	return this;
+};
